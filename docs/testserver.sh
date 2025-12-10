83d3e4ddcc6dba3689d30ee68c1f4be193e9f109
Evanpie711 <101840311+Evanpie711@users.noreply.github.com>
2025-12-10 08:30:16 -0700

    script update

--- docs/testserver.sh
+++ docs/testserver.sh
@@ -1,1 +1,8 @@
-python3 -m http.server --cgi 8000 -d /media/gamedisk/LinuxApps/EvansPortal2Tutoral/docs/
+#!/bin/bash
+
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+SCRIPT_DIR2=$SCRIPT_DIR'/'
+echo "script directory: $SCRIPT_DIR2"
+
+python3 -m http.server --cgi 8001 -d $SCRIPT_DIR2
