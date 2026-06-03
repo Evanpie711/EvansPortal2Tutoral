8617c0c65d4dfe7d6f8d89c48a54052b0f11afb3
Evanpie711 <evanpie711@gmail.com>
2026-06-03 15:28:57 +0100

    it no work

---
+++ docs/scripts/handle.js
@@ -0,0 +1,34 @@
+const sections = document.querySelectorAll('section')
+
+sections.forEach((section) => {
+  const handle = section.querySelector('a.handle')
+  const content = section.querySelector('div.content')
+
+  handle.addEventListener('mousedown', (event) => {
+    event.preventDefault()
+
+    const drag = (event) => {
+      event.preventDefault()
+
+      const left = (100 * event.pageX) / window.innerWidth
+      const right = 100 - left
+
+      content.style.gridTemplateColumns = `minmax(400px, ${left}%) minmax(400px, ${right}%)`
+      handle.style.left = `max(400px, ${left}%)`
+    }
+
+    const mouseup = (event) => {
+      event.preventDefault()
+
+      document.removeEventListener('mousemove', drag)
+      document.removeEventListener('mouseup', mouseup)
+    }
+
+    document.addEventListener('mousemove', drag)
+    document.addEventListener('mouseup', mouseup)
+  })
+
+  handle.addEventListener('click', (event) => {
+    event.preventDefault()
+  })
+})
