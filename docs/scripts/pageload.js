0d5a1b1c213fdd94b5a656b9cdb4a0f4456dc738
Evanpie711 <101840311+Evanpie711@users.noreply.github.com>
2026-06-03 17:29:47 +0100

    fixed the thingy

--- docs/scripts/pageload.js
+++ docs/scripts/pageload.js
@@ -1,4 +1,40 @@
 
+function loadResize(){
+  var sections = document.querySelectorAll('section')
+  sections.forEach((section) => {
+    var handle = section.querySelector('a.handle')
+    var content = section.querySelector('div.content')
+
+    handle.addEventListener('mousedown', (event) => {
+      event.preventDefault()
+
+      const drag = (event) => {
+        event.preventDefault()
+
+        const left = (100 * event.pageX) / window.innerWidth
+        const right = 100 - left
+
+        content.style.gridTemplateColumns = `minmax(400px, ${left}%) minmax(400px, ${right}%)`
+        handle.style.left = `max(400px, ${left}%)`
+      }
+
+      const mouseup = (event) => {
+        event.preventDefault()
+
+        document.removeEventListener('mousemove', drag)
+        document.removeEventListener('mouseup', mouseup)
+      }
+
+      document.addEventListener('mousemove', drag)
+      document.addEventListener('mouseup', mouseup)
+    })
+
+    handle.addEventListener('click', (event) => {
+      event.preventDefault()
+    })
+  })
+}
+
 async function loadhtml(name) {
 	var loader = new XMLHttpRequest();
 	loader.open('GET', name, true);
@@ -6,8 +42,13 @@ async function loadhtml(name) {
 		if(this.readyState!==4) {return;}
 		if(this.status!==200) {return;}
 		document.getElementById('ContentContainer').innerHTML=this.responseText;
+		loadResize();
 		return 1;
 	}
 	loader.send();
 
+	
+
 }
+
+
