3b10a4fc7378bb7eaa1a57cd2a31a4d5a399eb00
Evanpie711 <101840311+Evanpie711@users.noreply.github.com>
2026-06-06 21:30:18 +0100

    list

--- docs/scripts/theme.js
+++ docs/scripts/theme.js
@@ -9,6 +9,7 @@ function setdark() {
     document.documentElement.style.setProperty('--color-Text', '#F1F5F9');
     document.documentElement.style.setProperty('--color-TextSubtle', '#94A3B8');
 
+    document.documentElement.style.setProperty('--color-link', '#d4da82');
 }
 function setlight() {
     document.documentElement.style.setProperty('--color-Backround', '#f0f4ff');
@@ -17,6 +18,8 @@ function setlight() {
     document.documentElement.style.setProperty('--color-Border', '#d4ddeb');
     document.documentElement.style.setProperty('--color-Text', '#000000');
     document.documentElement.style.setProperty('--color-TextSubtle', '#202327');
+
+    document.documentElement.style.setProperty('--color-link', '#8377ec');
 }
 
 function switchtheme(){
